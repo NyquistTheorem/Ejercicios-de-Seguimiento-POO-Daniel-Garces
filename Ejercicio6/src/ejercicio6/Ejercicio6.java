bccee0ffab8b6cf40245fa51e6b7f4b27d74d6a3
NyquistTheorem <dgarcesr@unal.edu.co>
2026-05-06 17:35:03 -0500

    Add files via upload

---
+++ Ejercicio6/src/ejercicio6/Ejercicio6.java
@@ -0,0 +1,18 @@
+package ejercicio6;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+
+        int y = 10;
+        int z = -6;
+        int x;
+
+        if (y > z) {
+            x = 1;
+        } else {
+            x = 2;
+        }
+
+        System.out.println(x);
+    }
+}
